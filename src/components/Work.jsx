76e7385b68ab7b9e4d6952cc54fbd92124bcbb36
Abbas Ali <abbasali5784@gmail.com>
2023-08-16 00:43:00 -0400

    Made changes to work section of website

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import mp3 from "../assets/mp3.png";
 
 const Work = () => {
   return (
@@ -10,12 +11,97 @@ const Work = () => {
           </p>
           <p className="mt-6">Check out some of my recent work!</p>
         </div>
+
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div project-div meenCutz">
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
               <span className="text-2xl font-bold text-white tracking -tracking-wider">
-                React Js Application
+                MeenCutz Booking App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://iridescent-alpaca-41669a.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="##">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div content-div project-div mp3Converter">
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
+              <span className="text-2xl font-bold text-white tracking -tracking-wider">
+                Youtube To Mp3 Converter
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://iridescent-alpaca-41669a.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="##">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div project-div meetApp">
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
+              <span className="flex justify-center text-2xl font-bold text-white tracking -tracking-wider">
+                Meet App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="##">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="##">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div project-div myFlix ">
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
+              <span className="flex justify-center text-2xl font-bold text-white tracking -tracking-wider">
+                MyFlix App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="##">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="##">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div project-div pokeDex">
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
+              <span className="flex justify-center text-2xl font-bold text-white tracking -tracking-wider">
+                PokeDex App
               </span>
               <div className="pt-8 text-center">
                 <a href="##">
