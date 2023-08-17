68006da5bc0bf279b2de7e7994d894a42b112a0a
Abbas Ali <abbasali5784@gmail.com>
2023-08-17 12:47:06 -0400

    Made more responseive design changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="w-full h-screen bg-[#0a192f]">
+    <div name="home" className="home-screen w-full h-screen bg-[#0a192f]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full animate-fadeInDown">
         <p className="text-pink-600 font-bold group-hover:animate-pulse">
